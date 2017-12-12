a88ad0197a9c1315d9136e4efbebc8400ca2cf5a
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-12 13:43:54 +0100

    Création de l'exercice 5

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 5</title>
+</head>
+<body>
+  <?php
+  $genre = 'femme';
+  if($genre == 'femme') {
+    echo 'C\'est une développeuse !';
+  }
+  else {
+    echo 'C\'est un développeur !';
+  }
+  ?>
+</body>
+</html>
